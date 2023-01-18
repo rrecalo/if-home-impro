39a04060e34913f333b871cf01200e34b3a2f9a2
Robert Recalo <rrecalo3@ggc.edu>
2023-01-18 00:42:49 -0500

    Tweak styling of mobile navbar, add animation to open/close

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,66 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import {ReactComponent as Logo} from './logo.svg';
 import {FiMenu} from 'react-icons/fi'
+import {motion} from 'framer-motion'
+import {useAnimationControls} from 'framer-motion'
+
+var menuVariants = {
+  shown:{
+    x:0,
+    transition:{
+      duration: 0.5, type: "tween"
+    }
+  },
+  notShown:{
+    x:300,
+    transition:{
+      duration: 0.5, type: "tween"
+    }
+  }
+}
 
 const Navbar = () => {
 
   const [shown, setShown] = useState(false);
+  const controls = useAnimationControls("notShown");
+  const ref = useRef(null);
+  useOutsideAlerter(ref, shown);
 
   function toggleMenu(){
-    setShown(!shown);
+    if(shown){
+      controls.start("notShown");
+      setTimeout(()=>{
+        setShown(false);
+      }, 500);
+    }
+    else{
+      setShown(true);
+      controls.start("shown");
+    }
+  }
+
+  /**
+ * Hook that alerts clicks outside of the passed ref
+ */
+  function useOutsideAlerter(ref, shown) {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target) && shown) {
+          toggleMenu();
+        }
+      }
+      // Bind the event listener
+      document.body.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.body.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref, shown]);
   }
 
   return (
@@ -37,8 +88,11 @@ const Navbar = () => {
         <FiMenu className='w-10 h-10' onClick={toggleMenu}/>
       </div>
 
-      <div id="side_nav" className={"fixed top-[80px] flex-col right-0 w-1/2 bg-neutral-50 h-full justify-start items-left text-2xl font-light gap-4 p-1 pt-[25px] " + (shown ? 'flex' : 'hidden')}
-      onClick={toggleMenu}>
+      <motion.div id="side_nav" ref={ref} className={"fixed top-[80px] flex-col right-0 w-2/6 bg-neutral-100 opacity-95 h-full justify-start items-left text-2xl font-light gap-4 p-1 pl-2 pt-[25px] " + (shown ? 'flex' : 'hidden')}
+      onClick={toggleMenu}
+      variants={menuVariants}
+      initial={"notShown"}
+      animate={controls}>
       <Link to="/about" className='py-2'>
         About
       </Link>
@@ -51,7 +105,7 @@ const Navbar = () => {
       <Link to="/gallery" className='py-2'>
         Gallery
       </Link>
-      </div>
+      </motion.div>
 
       </div>
     </div>
